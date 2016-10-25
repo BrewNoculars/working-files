785c62bbc3138235edc652a7b63c6eb4eb4df435
janel lucia <thejanellucia@gmail.com>
2016-10-25 18:59:16 -0400

    starting work on breweryDB

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -2,6 +2,8 @@ var brewNoculars = {};
 
 var breweriesNearBy = {};
 
+
+//FourSquare API Starts here!
 breweriesNearBy.getBreweries = function (userLocation) {
 	$.ajax ({
 		url:'https://api.foursquare.com/v2/venues/search?client_id=XC45QHEBXODZWSFXRYRBKJCGDNOXYMLR14155RH1SXZ0CPIC&client_secret=BVUIPRJESP1EX4L0GLBO4VLDV0EEIYABKBS0KJOTUFCWV143&v=20160730',
@@ -17,3 +19,25 @@ breweriesNearBy.getBreweries = function (userLocation) {
 		console.log(brewery);
 	})
 }
+
+//Brewery DB API starts here!
+breweriesNearBy.getInfo = function () {
+	$.ajax ({
+		url: 'http://proxy.hackeryou.com',
+		method: 'GET',
+		dataType: 'json',
+		data:{
+			reqUrl: 'http://api.brewerydb.com/v2/search/geo/point/',
+			params: {
+				lat: 43.6532,
+				lng: 79.3832,
+				key: '3dae318cdfd5f407dccf3b5974924616'
+			}
+		}
+
+	}).then (function(bInfo){
+		console.log("is this working?", bInfo);
+	})
+}
+
+console.log('is this working?')
