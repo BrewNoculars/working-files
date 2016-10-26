02a9d653855c917b0d4328d7a833e6ecdf4263bd
Heather Pierce <pierce.heather.anne@gmail.com>
2016-10-26 14:49:43 -0400

    adding the AJAX object data for brewery DB

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -17,11 +17,12 @@ breweriesNearBy.getBreweries = function (userLocation) {
 		}
 	}).then (function(brewery){
 		console.log(brewery);
+
 	})
 }
 
 // Brewery DB API starts here!
-breweriesNearBy.getInfo = function () {
+breweriesNearBy.getInfo = function (latitude, longitude) {
 	$.ajax ({
 		url: 'http://proxy.hackeryou.com',
 		method: 'GET',
@@ -31,11 +32,21 @@ breweriesNearBy.getInfo = function () {
 			params: {
 				lat:'43.6532',
 				lng:'-79.3832',
+				radius:5,
 				key: '3dae318cdfd5f407dccf3b5974924616'
 			}
 		}
-
-	}).then (function(bInfo){
-		console.log(bInfo);
+	}).then(function(bInfo){
+		var brewerySpecifics = bInfo.data;
+		console.log(brewerySpecifics);
+		brewerySpecifics.forEach(function(bData){
+			console.log(bData);
+			var $bImages = bData.brewery.images.medium; //some breweries do not have images, so if erroring to undefined, then we need to code it to the image placeholder path
+			var $bName = bData.brewery.name;
+			var $bEstablished = bData.brewery.established;
+			var $bOrganic = bData.brewery.isOrganic;
+			var $bDescription = bData.brewery.description;
+		// console.log($bDescription,$bName,$bOrganic,$bImages,$bEstablished);
+		});
 	})
 }
